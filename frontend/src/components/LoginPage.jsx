aee0159bb325a638ead9bb7a1e53ff7934c821c0
Andrey Anisimov <maddbuzz@gmail.com>
2022-12-17 15:50:33 +0800

    work on 5th step

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
-import { useFormik } from 'formik';
+import { withFormik } from 'formik';
 import React, { useEffect, useRef, useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
+import * as Yup from 'yup';
 
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
@@ -9,12 +10,102 @@ import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
+import Image from 'react-bootstrap/Image';
 import Row from 'react-bootstrap/Row';
 import Stack from 'react-bootstrap/Stack';
-import Image from 'react-bootstrap/Image';
 
 import useAuth from '../hooks/index.jsx';
 import paths from '../paths.js';
+
+const FormContainer = ({ children }) => (
+  <Container fluid className="h-100">
+    <Row className="justify-content-center align-content-center h-100">
+      <Col xs="12" md="8" xxl="6">
+        <Card>
+          <Card.Body className="p-5">
+            <Row>
+              <Col className="d-flex align-items-center justify-content-center">
+                <Image src="login-image.jpg" alt="Войти" roundedCircle thumbnail />
+              </Col>
+              <Col>
+                {children}
+              </Col>
+            </Row>
+          </Card.Body>
+          <Card.Footer>
+            <div className="text-center">
+              <span>Нет аккаунта? </span>
+              <Link to="/signup">Регистрация</Link>
+            </div>
+          </Card.Footer>
+        </Card>
+      </Col>
+    </Row>
+  </Container>
+);
+
+const MyForm = (props) => {
+  const {
+    values,
+    touched,
+    errors,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    isSubmitting,
+    authFailed,
+    inputRef,
+  } = props;
+  return (
+    <Form onSubmit={handleSubmit}>
+      <h1 className="text-center mb-4">Войти</h1>
+      <fieldset disabled={isSubmitting}>
+        <Stack gap={4}>
+          <FloatingLabel label="Имя пользователя">
+            <Form.Control
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.username}
+              placeholder="Имя пользователя"
+              name="username"
+              id="username"
+              autoComplete="username"
+              isInvalid={touched.username && errors.username}
+              ref={inputRef}
+            />
+            <Form.Control.Feedback type="invalid" tooltip>
+              {errors.username}
+            </Form.Control.Feedback>
+          </FloatingLabel>
+          <FloatingLabel label="Пароль">
+            <Form.Control
+              type="password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.password}
+              placeholder="Пароль"
+              name="password"
+              id="password"
+              autoComplete="current-password"
+              isInvalid={authFailed}
+            />
+            <Form.Control.Feedback type="invalid" tooltip>
+              Неверные имя пользователя или пароль
+            </Form.Control.Feedback>
+          </FloatingLabel>
+          <Button type="submit" variant="outline-primary">Войти</Button>
+        </Stack>
+      </fieldset>
+    </Form>
+  );
+};
+
+const validationSchema = Yup.object().shape({
+  username: Yup.string().trim()
+    .min(3, 'От 3 до 20 символов')
+    .max(20, 'От 3 до 20 символов')
+    .required('Обязательное поле'),
+});
 
 const LoginPage = () => {
   const auth = useAuth();
@@ -23,93 +114,38 @@ const LoginPage = () => {
   const location = useLocation();
   const navigate = useNavigate();
   useEffect(() => {
-    inputRef.current.focus();
-  }, []);
+    inputRef.current?.focus();
+  });
 
-  const formik = useFormik({
-    initialValues: {
+  const MyEnhancedForm = withFormik({
+    mapPropsToValues: () => ({
       username: '',
       password: '',
-    },
-    onSubmit: async (values) => {
+    }),
+    validationSchema,
+    handleSubmit: async (values) => { // (values, { setSubmitting }) => {
       setAuthFailed(false);
-
       try {
         const res = await axios.post(paths.loginPath(), values);
         auth.userLogIn(res.data);
         const { from } = location.state || { from: { pathname: '/' } };
         navigate(from);
       } catch (err) {
-        formik.setSubmitting(false);
-        if (err.isAxiosError && err.response.status === 401) {
+        //  setSubmitting(false); If async Formik will automatically set isSubmitting to false...
+        if (err.isAxiosError) {
           setAuthFailed(true);
-          inputRef.current.select();
+          inputRef.current?.select();
           return;
         }
         throw err;
       }
     },
-  });
+  })(MyForm);
 
   return (
-    <Container fluid className="h-100">
-      <Row className="justify-content-center align-content-center h-100">
-        <Col xs="12" md="8" xxl="6">
-          <Card>
-            <Card.Body className="p-5">
-              <Row>
-                <Col className="d-flex align-items-center justify-content-center">
-                  <Image src="login-image.jpg" alt="Войти" roundedCircle thumbnail />
-                </Col>
-                <Col>
-                  <Form onSubmit={formik.handleSubmit}>
-                    <h1 className="text-center mb-4">Войти</h1>
-                    <fieldset disabled={formik.isSubmitting}>
-                      <Stack gap={2}>
-                        <FloatingLabel label="Ваш ник">
-                          <Form.Control
-                            onChange={formik.handleChange}
-                            value={formik.values.username}
-                            placeholder="Ваш ник"
-                            name="username"
-                            id="username"
-                            autoComplete="username"
-                            isInvalid={authFailed}
-                            required
-                            ref={inputRef}
-                          />
-                        </FloatingLabel>
-                        <FloatingLabel label="Пароль">
-                          <Form.Control
-                            type="password"
-                            onChange={formik.handleChange}
-                            value={formik.values.password}
-                            placeholder="Пароль"
-                            name="password"
-                            id="password"
-                            autoComplete="current-password"
-                            isInvalid={authFailed}
-                            required
-                          />
-                        </FloatingLabel>
-                        <Form.Control.Feedback type="invalid">the username or password is incorrect</Form.Control.Feedback>
-                        <Button type="submit" variant="outline-primary">Войти</Button>
-                      </Stack>
-                    </fieldset>
-                  </Form>
-                </Col>
-              </Row>
-            </Card.Body>
-            <Card.Footer>
-              <div className="text-center">
-                <span>Нет аккаунта? </span>
-                <Link to="/signup">Регистрация</Link>
-              </div>
-            </Card.Footer>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+    <FormContainer>
+      <MyEnhancedForm authFailed={authFailed} inputRef={inputRef} />
+    </FormContainer>
   );
 };
 
--- frontend/src/components/SignupPage.jsx
+++ frontend/src/components/SignupPage.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
-import { useFormik } from 'formik';
+import { withFormik } from 'formik';
 import React, { useEffect, useRef, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-// import * as Yup from 'yup';
+import * as Yup from 'yup';
 
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
@@ -17,120 +17,151 @@ import Stack from 'react-bootstrap/Stack';
 import useAuth from '../hooks/index.jsx';
 import paths from '../paths.js';
 
+const FormContainer = ({ children }) => (
+  <Container fluid className="h-100">
+    <Row className="justify-content-center align-content-center h-100">
+      <Col xs="12" md="8" xxl="6">
+        <Card>
+          <Card.Body className="p-5">
+            <Row>
+              <Col className="d-flex align-items-center justify-content-center">
+                <Image src="avatar_1.6084447160acc893a24d.jpg" alt="Регистрация" roundedCircle thumbnail />
+              </Col>
+              <Col>
+                {children}
+              </Col>
+            </Row>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  </Container>
+);
+
+const MyForm = (props) => {
+  const {
+    values,
+    touched,
+    errors,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    isSubmitting,
+    signupFailed,
+    inputRef,
+  } = props;
+  return (
+    <Form onSubmit={handleSubmit}>
+      <h1 className="text-center mb-4">Регистрация</h1>
+      <fieldset disabled={isSubmitting}>
+        <Stack gap={2}>
+          <FloatingLabel label="Имя пользователя">
+            <Form.Control
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.username}
+              placeholder="Имя пользователя"
+              name="username"
+              id="username"
+              autoComplete="username"
+              isInvalid={signupFailed || (touched.username && errors.username)}
+              ref={inputRef}
+            />
+            <Form.Control.Feedback type="invalid" tooltip>
+              {(signupFailed && 'Такой пользователь уже существует') || errors.username}
+            </Form.Control.Feedback>
+          </FloatingLabel>
+          <FloatingLabel label="Пароль">
+            <Form.Control
+              type="password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.password}
+              placeholder="Пароль"
+              name="password"
+              id="password"
+              autoComplete="current-password"
+              isInvalid={touched.password && errors.password}
+            />
+            <Form.Control.Feedback type="invalid" tooltip>
+              {errors.password}
+            </Form.Control.Feedback>
+          </FloatingLabel>
+          <FloatingLabel label="Подтвердите пароль">
+            <Form.Control
+              type="password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.passwordConfirmation}
+              placeholder="Подтвердите пароль"
+              name="passwordConfirmation"
+              id="passwordConfirmation"
+              autoComplete="current-passwordConfirmation"
+              isInvalid={touched.passwordConfirmation && errors.passwordConfirmation}
+            />
+            <Form.Control.Feedback type="invalid" tooltip>
+              {errors.passwordConfirmation}
+            </Form.Control.Feedback>
+          </FloatingLabel>
+          <Button type="submit" variant="outline-primary">Зарегистрироваться</Button>
+        </Stack>
+      </fieldset>
+    </Form>
+  );
+};
+
+const validationSchema = Yup.object().shape({
+  username: Yup.string().trim()
+    .min(3, 'От 3 до 20 символов')
+    .max(20, 'От 3 до 20 символов')
+    .required('Обязательное поле'),
+  password: Yup.string().trim()
+    .min(6, 'Не менее 6 символов')
+    .required('Обязательное поле'),
+  passwordConfirmation: Yup.string()
+    .oneOf([Yup.ref('password'), null], 'Пароли должны совпадать')
+    .required('Обязательное поле'),
+});
+
 const SignupPage = () => {
   const auth = useAuth();
-  const [authFailed, setAuthFailed] = useState(false);
+  const [signupFailed, setSignupFailed] = useState(false);
   const inputRef = useRef();
   const location = useLocation();
   const navigate = useNavigate();
   useEffect(() => {
-    inputRef.current.focus();
-  }, []);
+    inputRef.current?.focus();
+  });
 
-  // const SignupSchema = Yup.object().shape({
-  //   firstName: Yup.string()
-  //     .min(2, 'Too Short!')
-  //     .max(50, 'Too Long!')
-  //     .required('Required'),
-  //   lastName: Yup.string()
-  //     .min(2, 'Too Short!')
-  //     .max(50, 'Too Long!')
-  //     .required('Required'),
-  //   email: Yup.string().email('Invalid email').required('Required'),
-  // });
-
-  const formik = useFormik({
-    initialValues: {
+  const MyEnhancedForm = withFormik({
+    mapPropsToValues: () => ({
       username: '',
       password: '',
       passwordConfirmation: '',
-    },
-    onSubmit: async (values) => {
-      setAuthFailed(false);
-
+    }),
+    validationSchema,
+    handleSubmit: async (values) => {
+      setSignupFailed(false);
       try {
-        const res = await axios.post(paths.loginPath(), values);
+        const res = await axios.post(paths.signupPath(), values);
         auth.userLogIn(res.data);
         const { from } = location.state || { from: { pathname: '/' } };
         navigate(from);
       } catch (err) {
-        formik.setSubmitting(false);
-        if (err.isAxiosError && err.response.status === 401) {
-          setAuthFailed(true);
-          inputRef.current.select();
+        if (err.isAxiosError) {
+          setSignupFailed(true);
+          inputRef.current?.select();
           return;
         }
         throw err;
       }
     },
-  });
+  })(MyForm);
 
   return (
-    <Container fluid className="h-100">
-      <Row className="justify-content-center align-content-center h-100">
-        <Col xs="12" md="8" xxl="6">
-          <Card>
-            <Card.Body className="p-5">
-              <Row>
-                <Col className="d-flex align-items-center justify-content-center">
-                  <Image src="avatar_1.6084447160acc893a24d.jpg" alt="Регистрация" roundedCircle thumbnail />
-                </Col>
-                <Col>
-                  <Form onSubmit={formik.handleSubmit}>
-                    <h1 className="text-center mb-4">Регистрация</h1>
-                    <fieldset disabled={formik.isSubmitting}>
-                      <Stack gap={2}>
-                        <FloatingLabel label="Имя пользователя">
-                          <Form.Control
-                            onChange={formik.handleChange}
-                            value={formik.values.username}
-                            placeholder="Имя пользователя"
-                            name="username"
-                            id="username"
-                            autoComplete="username"
-                            isInvalid={authFailed}
-                            required
-                            ref={inputRef}
-                          />
-                        </FloatingLabel>
-                        <FloatingLabel label="Пароль">
-                          <Form.Control
-                            type="password"
-                            onChange={formik.handleChange}
-                            value={formik.values.password}
-                            placeholder="Пароль"
-                            name="password"
-                            id="password"
-                            autoComplete="current-password"
-                            isInvalid={authFailed}
-                            required
-                          />
-                        </FloatingLabel>
-                        <FloatingLabel label="Подтвердите пароль">
-                          <Form.Control
-                            type="password"
-                            onChange={formik.handleChange}
-                            value={formik.values.passwordConfirmation}
-                            placeholder="Подтвердите пароль"
-                            name="passwordConfirmation"
-                            id="passwordConfirmation"
-                            autoComplete="current-passwordConfirmation"
-                            isInvalid={authFailed}
-                            required
-                          />
-                        </FloatingLabel>
-                        <Form.Control.Feedback type="invalid">the username or password is incorrect</Form.Control.Feedback>
-                        <Button type="submit" variant="outline-primary">Зарегистрироваться</Button>
-                      </Stack>
-                    </fieldset>
-                  </Form>
-                </Col>
-              </Row>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+    <FormContainer>
+      <MyEnhancedForm signupFailed={signupFailed} inputRef={inputRef} />
+    </FormContainer>
   );
 };
 
